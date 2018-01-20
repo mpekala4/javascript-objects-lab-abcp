4012d7ee31032bb8a9edd6c913bf2fae74642663
Mike Pekala <mpekala411@gmail.com>
2018-01-20 03:02:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = new Object ({prop:'1'});
+
+function updateObjectWithKeyAndValue(object,key,value){
+  var recipe2 = Object.assign({},recipes,{prop2:"2"});
+  return recipe2
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+recipes.prop2='2'
+console.log(recipes)
+}
