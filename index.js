bfbf61b808454ba312fdd379c9b7f2d6ac34c46a
Mike Pekala <mpekala411@gmail.com>
2018-01-20 03:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function updateObjectWithKeyAndValue(object,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-var recipes2 = recipes.prop2="2"
-return recipes2 
+  recipes[prop2] = "2"
 }
